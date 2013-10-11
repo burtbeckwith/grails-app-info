00b5dbbf9b1b49c14cbbe807a7051ed5c97842c7
Burt Beckwith <burt@burtbeckwith.com>
2013-10-11 10:27:49 -0400

    plugin updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,4 @@
 grails.project.work.dir = 'target'
-grails.project.source.level = 1.6
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
 grails.project.dependency.resolution = {
@@ -9,18 +8,14 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
-		mavenLocal()
-		mavenCentral()
 	}
-
-	dependencies {}
 
 	plugins {
 		compile ':dynamic-controller:0.4'
-		compile ':google-visualization:0.5.5'
-		compile ':jquery:1.8.0'
+		compile ':google-visualization:0.6.2'
+		compile ':jquery:1.10.2'
 
-		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 	}
